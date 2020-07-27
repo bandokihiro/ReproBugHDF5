e35610459dcf6aed152cbba7c6c2c03e35655e6c
bandokihiro <bando.kihiro@gmail.com>
2020-07-27 08:11:03 -0700

    Add USE_DCR define

--- configure.sh
+++ configure.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 BUILD_TYPE=Debug
-HDF5_ROOT=/usr/lib/x86_64-linux-gnu/hdf5/serial
 LEGION_ROOT=/home/kihiro/Softwares/legion_DCR_debug
 
 cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
-      -DHDF5_ROOT=$HDF5_ROOT \
+      -DHDF5_ROOT="" \
       -DLegion_ROOT=$LEGION_ROOT \
       ..
