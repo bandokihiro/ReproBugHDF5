cf923a50afc186b5be24e002b5f470549d8161df
Kihiro Bando <bandokihiro@sapling.stanford.edu>
2020-07-28 14:30:52 -0700

    Make it work with cmake-8

--- configure.sh
+++ configure.sh
@@ -2,12 +2,13 @@
 
 BUILD_TYPE=Debug
 # LEGION_ROOT=/home/kihiro/Softwares/legion_DCR_debug
-LEGION_ROOT=/home/bandokihiro/Softwares/legion_DCR_debug
+# LEGION_ROOT=/home/bandokihiro/Softwares/legion_DCR_debug
+LEGION_ROOT=/home/bandokihiro/Softwares/legion_DCR_debug/share/Legion/cmake
 
 cmake \
     -DCMAKE_C_COMPILER="gcc-8" \
     -DCMAKE_CXX_COMPILER="g++-8" \
     -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
-    -DHDF5_ROOT="" \
-    -DLegion_ROOT=$LEGION_ROOT \
+    -DHDF5_DIR="" \
+    -DLegion_DIR=$LEGION_ROOT \
     ..
